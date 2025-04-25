c64cf4ce4e9c111a4b1c2836b576641cab8b44f9
Riley Price <rprice7443@gmail.com>
2025-04-24 22:49:09 -0700

    add git aliases

--- home/configs/git.nix
+++ home/configs/git.nix
@@ -2,6 +2,14 @@
 {
 programs.git = {
     enable = true;
+    aliases = {
+      "ps" = "push";
+      "a" = "add .";
+      "st" = "status";
+      "pl" = "pull";
+      "sw" = "switch";
+      "sc" = "switch -c";
+    };
     extraConfig = {
       user.name = "Riley Price";
       user.email = "rprice7443@gmail.com";
