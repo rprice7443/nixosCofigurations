a57fdb06a046cac5a8949e5b3daaa97596e7e6d0
Riley Price <rprice7443@gmail.com>
2025-05-23 00:25:12 -0700

    changes

--- home/features/desktop/gnome.nix
+++ home/features/desktop/gnome.nix
@@ -2,6 +2,11 @@
 
 with lib.hm.gvariant;
 {
+  services.dbus = {
+    enable = true;
+    packages = [ pkgs.gnome3.dconf ];
+  };
+
   dconf.settings = {
     "org/gnome/desktop/input-sources" = {
       show-all-sources = true;
