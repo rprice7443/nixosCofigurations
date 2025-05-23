b6f4d699162d3d69d2c0878ef14e1df0fd2f0de0
Riley Price <rprice7443@gmail.com>
2025-05-22 23:30:31 -0700

    new nixfmt

--- home/features/cli/helix.nix
+++ home/features/cli/helix.nix
@@ -21,7 +21,7 @@
       {
         name = "nix";
         auto-format = true;
-        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+        formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
       }
       {
         name = "go";
