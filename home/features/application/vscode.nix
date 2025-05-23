9113f3810f9618f6ca5c2c24698806bf21e78026
Riley Price <rprice7443@gmail.com>
2025-05-23 08:28:30 -0700

    change vscode settings

--- home/features/application/vscode.nix
+++ home/features/application/vscode.nix
@@ -2,5 +2,15 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    mutableExtensionsDir = false;
+    profiles = [ default ];
+
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+        vscodevim.vim
+        golang.go
+      ]
+    };
   };
 }
