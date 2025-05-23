a2eabd022dd321aa7605a1d866589b5b170421ef
Riley Price <rprice7443@gmail.com>
2025-05-23 08:52:47 -0700

    unbungle

--- home/features/application/vscode.nix
+++ home/features/application/vscode.nix
@@ -2,15 +2,21 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      vscodevim.vim 
+      golang.go
+    ];
+    
     # mutableExtensionsDir = false;
     # profiles = [ default ];
 
-    profiles.default = {
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        vscodevim.vim
-        golang.go
-      ];
-    };
+    #profiles.default = {
+    #  extensions = with pkgs.vscode-extensions; [
+    #    bbenoist.nix
+    #    vscodevim.vim
+    #    golang.go
+    #  ];
+    #};
   };
 }
