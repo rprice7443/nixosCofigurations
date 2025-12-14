13840c9fea715de8a7958720a537c88b7d706abe
Riley Price <rprice7443@gmail.com>
2025-12-14 13:21:22 -0800

    CHANGES

--- home/features/cli/git.nix
+++ home/features/cli/git.nix
@@ -2,20 +2,21 @@
 {
   programs.git = {
     enable = true;
-    settings.aliases = {
-      "ps" = "push";
-      "a" = "add .";
-      "st" = "status";
-      "pl" = "pull";
-      "sw" = "switch";
-      "sc" = "switch -c";
-      "c" = "commit -m";
-    };
-    extraConfig = {
+    settings = {
       user.name = "Riley Price";
       user.email = "rprice7443@gmail.com";
       init.defaultBranch = "main";
       push.autoSetupRemote = "true";
+
+      aliases = {
+        "ps" = "push";
+        "a" = "add .";
+        "st" = "status";
+        "pl" = "pull";
+        "sw" = "switch";
+        "sc" = "switch -c";
+        "c" = "commit -m";
+      };
     };
   };
 }
