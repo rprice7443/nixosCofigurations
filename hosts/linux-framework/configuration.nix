99faeb64cc950a55b1b76540eac68005c2fa0207
Riley Price <rprice7443@gmail.com>
2026-01-31 12:32:32 -0800

    changes

--- hosts/linux-framework/configuration.nix
+++ hosts/linux-framework/configuration.nix
@@ -148,6 +148,8 @@
     packages = with pkgs; [ tree ];
   };
 
+  hardware.opengl.enable = true;
+
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
