3ef01e0ef303b647bda569953a6207fc78de2c0f
Riley Price <rprice7443@gmail.com>
2026-05-05 23:29:26 -0700

    try

--- home/home-configurations.nix
+++ home/home-configurations.nix
@@ -1,18 +1,27 @@
 flakeInputs:
 let
-  inherit (flakeInputs) self home-manager nixpkgs;
+  inherit (flakeInputs)
+    self
+    home-manager
+    nixpkgs
+    noctalia
+    ;
 in
 {
-  "riley-framework" = let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
+  "riley-framework" =
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    in
+    home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [
+        self.homeManagerModules.framework
+        noctalia.homeManagerModules.default
+      ];
+
     };
-  in
-  home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
-
-    modules = [ self.homeManagerModules.framework ];
-
-  };
 }
