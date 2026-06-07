2c4ad008251b0974c1f32f7d31943a7163ab0f39
Riley Price <rprice7443@gmail.com>
2026-06-06 17:09:59 -0700

    add more abbrvz

--- home/cli/fish.nix
+++ home/cli/fish.nix
@@ -7,6 +7,7 @@
 {
   programs.fish = {
     enable = true;
+    preferAbbrs = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       starship init fish | source
@@ -14,6 +15,16 @@
       direnv hook fish | source
       mise activate fish | source
     '';
+    shellAbbrs = {
+      "gac" = "git add . && git commit -m";
+      "gps" = "git push";
+      "ssr" = "sudo systemctl restart";
+      "jfu" = "journalctl -f -u";
+      "nfu" = "nix flake update";
+      "ndd" = "nix develop .#";
+      "opc" = "opencode";
+      "zlj" = "zellij";
+    };
   };
 
   home.packages = with pkgs; [
